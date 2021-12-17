76fe1e98c6f4a6323ccf07075026f36489d4db23
Lucas Wang <88947823+Lucas-Rei@users.noreply.github.com>
2021-12-16 16:34:36 -0800

    chess v2, electric boogalo
    added movement, smart generation for the rook, capturing, and turns

--- chess.py
+++ chess.py
@@ -1,16 +1,26 @@
 from pygame import *
 import pygame
 
+def turn_flip():
+    global turn
+    if turn:
+        turn = False
+    
+    elif not turn:
+        turn = True
+
 # GameSprite class
 class GameSprite(sprite.Sprite): 
-    def __init__(self, sprite_image, size1, size2, coord, select = False):
+    def __init__(self, sprite_image, size1, size2, coord, color = "white", select = False):
         super().__init__()
         self.size1 = size1
         self.size2 = size2
         self.image = transform.scale(pygame.image.load(sprite_image), (self.size1, self.size2))
         self.rect = self.image.get_rect()
         self.coord = coord
+        self.color = color
         self.select = select
+        self.stop = 1
 
 
     def reset(self):
@@ -59,9 +69,14 @@ class MoveSpace(GameSprite):
         if pygame.mouse.get_pressed()[0] and self.rect.collidepoint(pygame.mouse.get_pos()):
             for piece in piece_group:
                 if piece.select == True:
+                    for piece2 in piece_group:
+                        if piece2.coord == self.coord:
+                            piece2.kill()
+                            # probably add capturing noises and other shenanegins
                     piece.coord = self.coord
                     piece.select = False
-                    self.coord[0] = -1
+                    sprite.Group.empty(moveSpace_group)
+                    turn_flip()
 
 
 class King(GameSprite):
@@ -78,24 +93,110 @@ class Knight(GameSprite):
 
 class Rook(GameSprite):
     def update(self):
-        # test shit
-        key_list = key.get_pressed()
-        if key_list[K_RIGHT]: 
-            self.coord[0] += 1
-        if key_list[K_LEFT]: 
-            self.coord[0] -= 1
-        if key_list[K_UP]:
-            self.coord[1] -= 1
-        if key_list[K_DOWN]:
-            self.coord[1] += 1
-
-        if pygame.mouse.get_pressed()[0] and self.rect.collidepoint(pygame.mouse.get_pos()):
+        # generating movespaces
+        if pygame.mouse.get_pressed()[0] and self.rect.collidepoint(pygame.mouse.get_pos()) and not self.select:
             self.select = True
 
-            print("ready to move")
+            # generate left
+            x_var = self.coord[0] - 1
+            self.stop = 1
+            for i in range(7):
+                if self.stop == 3:
+                    break
 
-            for i in range(8):
-                pass
+                # check for collisions with own pieces
+                for piece in piece_group:
+                    if piece.coord == [x_var, self.coord[1]]:
+                        if piece.color == self.color:
+                            self.stop = 2
+
+                        elif piece.color != self.color:
+                            self.stop = 3
+                        
+
+                if x_var < 0 or self.stop == 2:
+                    break   
+
+
+                new = MoveSpace("circle.png", 60, 60, [x_var, self.coord[1]])
+                moveSpace_group.add(new)
+                x_var -= 1
+
+            # generate up
+            y_var = self.coord[1] + 1
+            self.stop = 1
+            for i in range(7):
+                if self.stop == 3:
+                    break
+
+                # check for collisions with own pieces
+                for piece in piece_group:
+                    if piece.coord == [self.coord[0], y_var]:
+                        if piece.color == self.color:
+                            self.stop = 2
+
+                        elif piece.color != self.color:
+                            self.stop = 3
+                        
+
+                if y_var > 7 or self.stop == 2:
+                    break   
+
+
+                new = MoveSpace("circle.png", 60, 60, [self.coord[0], y_var])
+                moveSpace_group.add(new)
+                y_var += 1
+
+            # generate right
+            x_var = self.coord[0] + 1
+            self.stop = 1
+            for i in range(7):  
+
+                if self.stop == 3:
+                    break
+
+                # check for collisions with own pieces
+                for piece in piece_group:
+                    if piece.coord == [x_var, self.coord[1]]:
+                        if piece.color == self.color:
+                            self.stop = 2
+
+                        elif piece.color != self.color:
+                            self.stop = 3
+                        
+
+                if x_var > 7 or self.stop == 2:
+                    break             
+                        
+
+                new = MoveSpace("circle.png", 60, 60, [x_var, self.coord[1]])
+                moveSpace_group.add(new)
+                x_var += 1
+
+            # generate down
+            y_var = self.coord[1] - 1
+            self.stop = 1
+            for i in range(7):
+                if self.stop == 3:
+                    break
+
+                # check for collisions with own pieces
+                for piece in piece_group:
+                    if piece.coord == [self.coord[0], y_var]:
+                        if piece.color == self.color:
+                            self.stop = 2
+
+                        elif piece.color != self.color:
+                            self.stop = 3
+                        
+
+                if y_var < 0 or self.stop == 2:
+                    break   
+
+
+                new = MoveSpace("circle.png", 60, 60, [self.coord[0], y_var])
+                moveSpace_group.add(new)
+                y_var -= 1
 
 class Pawn(GameSprite):
     pass
@@ -104,29 +205,37 @@ class Pawn(GameSprite):
 # creation
 
 piece_group = sprite.Group()
-MoveSpace_list = sprite.Group()
+white_sub = sprite.Group()
+black_sub = sprite.Group()
+moveSpace_group = sprite.Group()
 
-window = display.set_mode((600, 600))
+window = display.set_mode((640, 640))
 display.set_caption("Chess!")
 
-board = transform.scale(image.load("chess board sample.png"), (600, 600))
+board = transform.scale(image.load("chess board sample.png"), (640, 640))
 
-blackRook1 = Rook("black rook.png", 60, 60, [0, 0])
+blackRook1 = Rook("black rook.png", 60, 60, [4, 4], "black")
 piece_group.add(blackRook1)
+black_sub.add(blackRook1)
 
-test = MoveSpace("circle.png", 60, 60, [3,2])
+blackRook2 = Rook("black rook.png", 60, 60, [1, 4], "black")
+piece_group.add(blackRook2)
+black_sub.add(blackRook2)
 
-
+whiteRook1 = Rook("white rook.jfif", 60, 60, [6, 4], "white")
+piece_group.add(whiteRook1)
+white_sub.add(whiteRook1)
 
 
 # positions and other stuff
 
 run = True
+turn = False
 FPS = 60
 clock = time.Clock()
 
-x_positions = [10, 85, 160, 235, 310, 385, 460, 535]
-y_positions = [10, 85, 160, 235, 310, 385, 460, 535]
+x_positions = [10, 90, 170, 250, 330, 410, 490, 570]
+y_positions = [570, 490, 410, 330, 250, 170, 90, 10]
 
 
 
@@ -138,12 +247,20 @@ while run:
 
     #blit shite
     window.blit(board, (0, 0))
-    blackRook1.reset()
-    test.reset()
+    for piece in piece_group:
+        piece.reset()
+
+    for space in moveSpace_group:
+        space.reset()
 
     #updating
-    blackRook1.update()
-    test.update()
+    if turn:
+        white_sub.update()
+
+    if not turn:
+        black_sub.update()
+
+    moveSpace_group.update()
 
     pygame.display.update()
     clock.tick(FPS)
